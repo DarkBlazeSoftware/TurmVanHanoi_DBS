e91a226fd4a13554c7f8eaac78941ac54c618d80
Encrypt0n <37348021+Encrypt0n@users.noreply.github.com>
2019-01-07 13:40:17 +0100

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorenvanHanoi
+{
+    class Calculator
+    {
+        public static int ParseDisks()
+        {
+            // Declaring variables
+            string input;
+            int disks;
+
+            // User input
+            do
+            {
+                Console.WriteLine("Enter an amount of disks (0-64): ");
+                input = Console.ReadLine();
+
+            } while (!int.TryParse(input, out disks) || disks < 0 || disks > 64);
+
+            // Calculation
+            return disks;
+        }
+
+
+
+
+
+    }
+}
---
+++ Forloop.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorenvanHanoi
+{
+    class Forloop
+    {
+        public static void Hanoi()
+        {
+
+
+
+
+
+
+
+            /*int invoer = Berekenaar.Parsedisks;
+
+            for (int disks = 0; disks <= input; disks++)
+
+            {
+                zetten = 0;
+
+                for (uint macht = 0; macht < disks; macht++)
+                {
+                    zetten = zetten * 2 + 1;
+
+                }
+
+            }*/
+
+            ulong movesholder;
+
+            int input = Calculator.ParseDisks();
+            
+            for (int disks = 0; disks <= input; disks++)
+            {
+
+
+                movesholder = 0;
+
+                for (uint i = 0; i <= input; i++)
+                {
+                   ulong moves = movesholder * 2 + 1;
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
+
